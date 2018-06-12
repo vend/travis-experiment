31fb2bdc0f0a3738e1116dbf497711b657c4fc53
Alexei Tenitski <alexei@vendhq.com>
2018-06-12 13:40:32 +1200

    Debugging

--- ci/test.sh
+++ ci/test.sh
@@ -4,5 +4,12 @@ set -e
 
 echo $CHANGED_DIRS
 
+echo "1"
+git diff --name-only master...
+
+echo "2"
+git diff --name-only origin/master...
+
+
 echo "$SERVICE is us! Calling $SERVICE/ci/test.sh"
 "services/"$SERVICE/ci/test.sh
