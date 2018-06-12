6866d78eb75c713370bf4072eba805d1963ec28a
Alexei Tenitski <alexei@vendhq.com>
2018-06-12 13:29:37 +1200

    Fixed changes script

--- ci/services.sh
+++ ci/services.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-CHANGED_DIRS=$(git diff --name-only master... | awk -F '/' '/.*\// {print $1 "/" $2 "/"}' | sort | uniq)
+CHANGED_DIRS=$(git diff --name-only master... | awk -F '/' '/.*\// {print $1 "/" $2 }' | sort | uniq)
 
+JOINED=""
 for CHANGED_DIR in $CHANGED_DIRS
 do
-    echo "<$CHANGED_DIR>"
+    JOINED="$JOINED<$CHANGED_DIR>"
 done
+
+echo $JOINED
