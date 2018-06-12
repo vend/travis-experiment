d5f61d960b37e5228e863dfd0da04f4fc9143f2c
Alexei Tenitski <alexei@vendhq.com>
2018-06-12 15:13:05 +1200

    Will work now!

--- ci/services.sh
+++ ci/services.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-CHANGED_DIRS111=$(git fetch origin master && git diff --name-only FETCH_HEAD... | awk -F '/' '/.*\// {print $1 "/" $2 }' | sort | uniq)
+CHANGED_DIRS=$(git fetch origin master && git diff --name-only FETCH_HEAD... | awk -F '/' '/.*\// {print $1 "/" $2 }' | sort | uniq)
 
 JOINED=""
-for CHANGED_DIR in $CHANGED_DIRS111
+for CHANGED_DIR in $CHANGED_DIRS
 do
     JOINED="$JOINED@$CHANGED_DIR@"
 done
--- ci/test.sh
+++ ci/test.sh
@@ -2,9 +2,5 @@
 
 set -e
 
-CHANGED_DIR10=$(./ci/services.sh)
-
-echo "****$CHANGED_DIR10****"
-
 echo "$SERVICE is us! Calling $SERVICE/ci/test.sh"
 "services/"$SERVICE/ci/test.sh
