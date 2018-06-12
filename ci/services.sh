d2ece8906c3f625538e9f80121161df07b3b54ab
Alexei Tenitski <alexei@vendhq.com>
2018-06-12 13:26:43 +1200

    Minor change

--- ci/services.sh
+++ ci/services.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CHANGED_DIRS=$(git diff --name-only master...  | awk -F '/' '/.*\// {print $1 "/" $2 "/"}' | sort | uniq)
+CHANGED_DIRS=$(git diff --name-only master... | awk -F '/' '/.*\// {print $1 "/" $2 "/"}' | sort | uniq)
 
 for CHANGED_DIR in $CHANGED_DIRS
 do
