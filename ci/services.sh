93a83ef4f89d10c8f4e4cc509cc2aa929d915c90
Alexei Tenitski <alexei@vendhq.com>
2018-06-12 13:35:06 +1200

    Escaping

--- ci/services.sh
+++ ci/services.sh
@@ -7,7 +7,7 @@ CHANGED_DIRS=$(git diff --name-only master... | awk -F '/' '/.*\// {print $1 "/"
 JOINED=""
 for CHANGED_DIR in $CHANGED_DIRS
 do
-    JOINED="$JOINED<$CHANGED_DIR>"
+    JOINED="$JOINED@$CHANGED_DIR@"
 done
 
 echo $JOINED
