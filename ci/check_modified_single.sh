22945a2a3de35156071e4d604f0bfef918238bfe
Alexei Tenitski <alexei@vendhq.com>
2018-06-28 19:47:52 +1200

    Undo lib change

--- ci/check_modified_single.sh
+++ ci/check_modified_single.sh
@@ -12,6 +12,7 @@ set -e
 MODIFIED_DIRS=$(./ci/modified_dirs.sh)
 echo -e "Directories with changes:\n$MODIFIED_DIRS\n"
 
+COMPONENT_TYPE=""
 COMPONENT_NAME=""
 THIS_MODIFIED=false
 OTHERS_MODIFIED=false
@@ -21,6 +22,7 @@ do
     if [[ ${TRAVIS_BRANCH/-//} =~ ^${MODIFIED_DIR}- ]] ; then
         THIS_MODIFIED=true
         PATH_PARTS=(${MODIFIED_DIR/\// })
+        COMPONENT_TYPE=${PATH_PARTS[0]}
         COMPONENT_NAME=${PATH_PARTS[1]}
     else
         OTHERS_MODIFIED=true
@@ -33,7 +35,7 @@ if [[ $OTHERS_MODIFIED == true ]] ; then
 elif [[ $THIS_MODIFIED == true ]] ; then
     # Check that the component is registered in travis file
     if grep -q "COMPONENT_NAME=${COMPONENT_NAME}" .travis.yml ; then
-        echo -e "Single component $TRAVIS_BRANCH was modified, PROCEED"
+        echo -e "Single component $COMPONENT_TYPE $COMPONENT_NAME was modified, PROCEED"
         exit 0
     else
         echo -e "Component modified in branch $TRAVIS_BRANCH is not registered in .travis.yml, FAIL BUILD"
